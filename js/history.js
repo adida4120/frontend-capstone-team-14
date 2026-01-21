733765b300215f9d207ee1172fdb873c1be09811
Adi Dahari <adidahari100@gmail.com>
2026-01-21 15:42:15 +0200

    feat(history): scaffold history.js with JSON seed and structure

--- js/history.js
+++ js/history.js
@@ -1,1 +1,87 @@
-"use strict";
+/* ==========
+   CONFIG
+========== */
+const STORAGE_KEY = "transactions";
+
+/* ==========
+   SEED
+========== */
+const seedTransactions = [
+    {id: "txt1", date: "05-01-2026", type: "expense", category: "Food", amount: 45, note:"Pizza"},
+    {id: "txt2", date: "10-01-2026", type: "icome", category: "Salary", amount: 10000, note:"December salery"},
+    { id: "tx3", date: "2026-01-12", type: "expense", category: "Transport", amount: 18, note: "Bus" },
+    { id: "tx4", date: "2026-01-19", type: "expense", category: "Shopping", amount: 210, note: "Skincare" }
+];
+/* ==========
+   DOM
+========== */
+const exportBtn = document.getElementById("exportBtn");
+const clearBtn = document.getElementById("clearBtn");
+const searchInput = document.getElementById("searchInput");
+const filterMode = document.getElementById("filterMode");
+
+const rangeWrap = document.getElementById("rangeWrap");
+const fromDate = document.getElementById("fromDate");
+const toDate = document.getElementById("toDate");
+
+const emptyState = document.getElementById("emptyState");
+const txList = document.getElementById("txList");
+
+const yearSpan = document.getElementById("yaer");
+
+/* ==========
+   STATE
+========== */
+let allTx = [];
+let filteredTx = [];
+
+/* ==========
+   INIT
+========== */
+
+init();
+
+function init(){
+    setYear();
+    ensureSeed();
+    allTx = loadTransactions();
+    bindEvents();
+    toggleRangeUI();
+    applyFiltersAndRender();
+
+
+}
+
+/* ==========
+   COMMON - take to COMMON.JS
+========== */
+function setYear(){
+    if (!localStorage.getItem(STORAGE_KEY)){
+        localStorage.setItem(STORAGE_KEY,JASON.stringify(seedTransactions));
+    }
+}
+
+function ensureSeed() {}
+
+/* ==========
+   STORAGE
+========== */
+function loadTransactions() {}
+
+
+/* ==========
+   EVENTS
+========== */
+function bindEvents() {}
+
+function toggleRangeUI() {}
+
+function applyFiltersAndRender() {}
+
+
+
+
+/* ==========
+   EXPORT CSV
+========== */
+function exportToCSV(arr) {}
