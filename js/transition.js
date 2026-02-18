2310a8497ca802d4ed9ee7684a4adc807d6d553f
Adi Dahari <adidahari100@gmail.com>
2026-02-18 18:24:52 +0200

    add: contact page functionality and layout styling; remove transition page

--- js/transition.js
+++
@@ -1,6 +0,0 @@
-let menuBtn= document.querySelector('.menu-toggle');
-let navMenu =document.querySelector('.main-nav');
-
-menuBtn.addEventListener('click', function(){
-    navMenu.classList.toggle('active');
-});
